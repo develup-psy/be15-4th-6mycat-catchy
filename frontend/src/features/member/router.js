c43b1959afe24f025ff8f42df8a9482a21a7c049
didiha <hcring@naver.com>
2025-05-27 10:25:28 +0900

    feat: 로그인 상태일 때 회원가입, 로그인 페이지 접근 금지

--- frontend/src/features/member/router.js
+++ frontend/src/features/member/router.js
@@ -3,14 +3,16 @@ import SignupExtraView from '@/features/member/views/SignupExtraView.vue';
 
 export const MemberRoutes = [
   {
-    path: '/member/start',
-    name: 'SignupView',
-    component: SignupView,
-  },
+  path: '/member/start',
+  name: 'SignupView',
+  component: SignupView,
+  meta: { guestOnly: true },
+},
   {
     path: '/member/start/extra',
     name: 'SignupExtraView',
     component: SignupExtraView,
+    meta: { guestOnly: true },
   },
   {
     path: '/member/info',
