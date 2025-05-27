1b1e0f3d56a4c9d5e77253c56242c327d32d382c
PSY <develup-psy@github.com>
2025-05-27 12:45:22 +0900

    test: 더미 데이터 추가

--- frontend/src/features/jjure/views/JjureListPage.vue
+++ frontend/src/features/jjure/views/JjureListPage.vue
@@ -15,6 +15,8 @@ const fetchFn = async (page) => {
     startLoading();
     const { data } = await fetchJjureList({ page: sendedPage, size });
     console.log("더미 데이터 추가")
+    console.log("더미 데이터 추가")
+    
     return data;
   } catch (e) {
     console.log(e + '알림 목록 초기 로드 실패');
